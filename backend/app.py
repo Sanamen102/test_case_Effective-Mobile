a41075d033fc6158f427a386e1159267fac317cd
std-ext-014-33 std-ext-014-33 <std-ext-014-33@praktikum-services.ru>
2026-03-25 00:30:15 +0500

    Написал простой HTTP-сервер на Python

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
-print('hello')
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hello from Effective Mobile!")
+
+if __name__ == "__main__":
+    server_address = ('', 8080)
+    httpd = HTTPServer(server_address, SimpleHandler)
+    httpd.serve_forever()
+    
